1009e2ace61d794aea288d7420ca6180e12ad4a9
BeafCultist <147671982+BeafCultist@users.noreply.github.com>
2026-04-04 17:38:12 -0400

    Bullet Functionality
    Bullets can now move and start on a delay

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,17 +1,27 @@
 extends Area2D
 
 @export var speed : float = 200.0
+@export_range(0, 360) var angle : float
 @export var ownerGroup : String
 @export var isParryable : bool
 @onready var destroyTimer : Timer = $DestroyTimer
+@onready var startTimer : Timer = $StartTimer
+@onready var moveDir : Vector2
 
-var moveDir : Vector2
+var timeToStart : bool = false
+
+func _ready():
+	moveDir = Vector2.RIGHT.rotated(deg_to_rad(angle))
 
 func _process (delta):
-	translate(moveDir * speed * delta)
+	if (timeToStart):
+		translate(moveDir * speed * delta)
 
 func _on_body_entered(body: Node2D) -> void:
 	queue_free()
+
+func _on_start_timer_timeout() -> void:
+	timeToStart = true
 
 func _on_destroy_timer_timeout() -> void:
 	queue_free()
