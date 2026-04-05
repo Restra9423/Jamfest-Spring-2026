2ffd9c7fc0ae05d73a437ae5144165f5429843ea
Restra9423 <jordan.d.mcg1l@gmail.com>
2026-04-05 03:55:12 -0400

    parry movement fix for real (final) last (1) (4).exe

--- Scripts/playerMovement.gd
+++ Scripts/playerMovement.gd
@@ -7,11 +7,12 @@ var health = 3
 var points = 0
 var iFramesActive = false
 @onready var iFrames : Timer = $iFrames
-var lastMousePos = [Vector2.ZERO, Vector2.ZERO, Vector2.ZERO]
+var avgMouseMove = [Vector2.ZERO, Vector2.ZERO, Vector2.ZERO]
+var lastMousePos = Vector2.ZERO
 @onready var parryPivot : Node2D = $ParryPivot
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
 
 func _process(delta: float) -> void:
 	if(health < 1):
@@ -21,10 +22,12 @@ func _process(delta: float) -> void:
 	velocity = lerp(velocity, direction * speed, lerpWeight)
 	move_and_slide()
 	
-	var parryDir = (lastMousePos[0]+lastMousePos[1]+lastMousePos[2])/3
-	parryPivot.rotation = lerp(velocity,parryDir.normalized(),lerpWeight).angle()
-	lastMousePos.append(get_global_mouse_position())
-	lastMousePos.pop_front()
+	var parryDir = (avgMouseMove[0]+avgMouseMove[1]+avgMouseMove[2])/3
+	if(get_global_mouse_position() - lastMousePos != Vector2.ZERO):
+		parryPivot.rotation = lerp(Vector2.ZERO,parryDir.normalized(),lerpWeight).angle()
+		avgMouseMove.append(get_global_mouse_position() - lastMousePos)
+		avgMouseMove.pop_front()
+	lastMousePos = get_global_mouse_position()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	hurt()
