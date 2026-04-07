55ae34b4ec07da4e9b21e8204d91da6502a434ab
BeafCultist <147671982+BeafCultist@users.noreply.github.com>
2026-04-07 16:22:03 -0400

    removing redundant code

--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -82,9 +82,6 @@ func _process(delta: float) -> void:
 
 
 #collision functions
-func _on_area_2d_body_entered(_body: Node2D) -> void:
-		hurt()
-
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if (area.is_in_group("bullets") && !area.parriedBullet):
 		hurt()
