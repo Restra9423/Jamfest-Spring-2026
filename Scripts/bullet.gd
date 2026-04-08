288c6114248c117b4e59037bc36795ffd0558ddd
Restra9423 <jordan.d.mcg1l@gmail.com>
2026-04-07 20:38:28 -0400

    parried bullets that parry unparryable bullets unless parried by previously parried bullets now properly count score :)

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -68,7 +68,9 @@ func setParried(parriedDir: Vector2):
 func _on_area_entered(area: Area2D) -> void:
 	if(area.is_in_group("bullets")):
 		if(parriedBullet && isParryable && !area.isParryable && !area.parriedBullet):
-			# ScoreCounter.incrementScore(10)
+			ScoreCounter.incrementScore(area.pointValue)
+			ScoreManager.instance.updateScore()
+			print("proof")
 			if (area.moveDir.x > moveDir.x):
 				if (area.moveDir.y > moveDir.y):
 					area.setParried(-(abs(area.moveDir * moveDir)))
--- Scripts/scoreManager.gd
+++ Scripts/scoreManager.gd
@@ -1,7 +1,13 @@
 extends Control
+class_name ScoreManager
 
 @export var scoreDisplay : Label
 @export var comboDisplay : Label
+
+static var instance: ScoreManager
+
+func _init() -> void:
+	instance = self
 
 func updateScore():
 	scoreDisplay.text = str(ScoreCounter.currentScore)
