9cc86b6fe2ff45bc24369f8cc1d57b53cda152e1
Restra9423 <jordan.d.mcg1l@gmail.com>
2026-05-08 01:03:26 -0400

    no title kill

--- UI/title.gd
+++ UI/title.gd
@@ -13,10 +13,10 @@ func _ready() -> void:
 		button.focus_entered.connect(_on_any_button_focused)
 		
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("Back"):
-		AudioController.playSFX(AudioController.clickSound)
-		get_tree().quit()
-	#
+	#if Input.is_action_just_pressed("Back"):
+		#AudioController.playSFX(AudioController.clickSound)
+		#get_tree().quit()
+	pass
 	#aimInput = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
 	#if aimInput != Vector2.ZERO:
 		#Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
